897684eab1177270f48325314d4f77d17dcd3274
eva miliczka <eva.miliczka@gmail.com>
2023-06-06 11:28:29 +0200

    Playing with exports

--- app.js
+++ app.js
@@ -5,30 +5,35 @@ const config = require('./utils/config');
 
 const app = express();
 const notesRouter = require('./controllers/notes');
-const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
-
+const {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+} = require('./utils/middleware');
+const { info, error } = require('./utils/logger');
 
 mongoose.set('strictQuery', false);
 
-logger.info('Connecting to ', config.MONGODB_URI);
+info('Connecting to ', config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {  // replaced result by ()
-    logger.info('Connected to MongoDB');
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    // replaced result by ()
+    info('Connected to MongoDB');
   })
-  .catch((error) => {
-    logger.error('Error connecting to MongoDB: ', error.message);
-});
+  .catch((err) => {
+    error('Error connecting to MongoDB: ', err.message);
+  });
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(requestLogger);
 
 app.use('/api/notes', notesRouter);
 
-app.use(middleware.unknownEndpoint); // no routes or middleware are called after this, with the exception of errorHandler
-app.use(middleware.errorHandler);
+app.use(unknownEndpoint); // no routes or middleware are called after this, with the exception of errorHandler
+app.use(errorHandler);
 
 module.exports = app;
